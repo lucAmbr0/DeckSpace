6db2c67b25ea4bdd7bc4b9002f033d3b6e2ea717
Luca <luca.ambrosonee@gmail.com>
2024-09-06 19:50:07 +0200

    Improved splashscreen animation

--- index.js
+++ index.js
@@ -24,22 +24,10 @@ function serviceWorker() {
 
 // ---------------  FUNCTIONS  ---------------
 
-// splashscreen();
+splashscreen();
 function splashscreen() {
-  const splashscreen =
-    `
-  <div id="splashScreenBox">
-  <img src="icons/icon-any-512x512.png" alt="App Logo">
-  <div id="splashScreenAppInfo">
-    <h1>DeckSpace</h1>
-    <h3>by lucAmbr0</h2>
-    <h4>vx.x.x</h4>
-  </div>
-  </div>
-  `
-  const page = document.body.innerHTML;
-  document.body.innerHTML = splashscreen;
   setTimeout(() => {
-    
-  }, 500);
+    document.getElementById("splashScreenBox").style.animation = "exitUp 0.4s ease-in-out forwards"
+  }, 1500);
+  
 }

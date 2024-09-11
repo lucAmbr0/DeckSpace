41257104dc5d1253d8e4eabb2f7b7805ae6dc183
Luca <luca.ambrosonee@gmail.com>
2024-09-11 11:52:10 +0200

    Improved micro-animation in balance/bets bottom bars

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function serviceWorker() {
 const startAnimation = true;
 
 
-// ---------------  FUNCTIONS  ---------------
+// ---------------  SPLASHSCREEN ANIMATION  ---------------
 
 splashscreen();
 function splashscreen() {
@@ -47,46 +47,81 @@ function splashscreen() {
 }
 
 
+// --------------- BALANCE / BETS - CONTROL BARS ---------------
+
+
+// BALANCE BAR VARIABLES
 const balanceControls = document.getElementById("balanceControlsContainer");
 const userBalanceBtn = document.getElementById("userBalanceBtn");
 const userBalanceAmountContainer = document.getElementById("userBalanceAmountContainer");
+const balanceButtons = balanceControls.querySelectorAll('button');
 
-function toggleBalanceControls() {
-  if (balanceControls.classList.contains("controlsHIDDEN")) {
-    if (!betControls.classList.contains("controlsHIDDEN")) {
-      betControls.classList.add("controlsHIDDEN");
-      userBetBtn.classList.remove("userBalanceBtnACTIVE");
-      userBetAmountContainer.style.animation = "none";
-    }
-    balanceControls.classList.remove("controlsHIDDEN");
-    userBalanceAmountContainer.style.animation = "balanceControlsEntrance forwards ease-out .3s"
-    userBalanceBtn.classList.add("userBalanceBtnACTIVE");
-  }
-  else {
-    balanceControls.classList.add("controlsHIDDEN");
-    userBalanceBtn.classList.remove("userBalanceBtnACTIVE");
-    userBalanceAmountContainer.style.animation = "none";
-  }
-}
-
+// BET BAR VARIABLES
 const betControls = document.getElementById("betControlsContainer");
 const userBetBtn = document.getElementById("userBetBtn");
 const userBetAmountContainer = document.getElementById("userBetAmountContainer");
+const betButtons = betControls.querySelectorAll('button');
+
+// ANIMATIONS VARIABLES
+const barAnimation = "balanceControlsEntrance forwards ease-in-out .3s";
+const buttonsAnimation = "buttonsZoomIn 0.2s ease-in-out forwards";
+
+function toggleBalanceControls() {
+  if (balanceControls.classList.contains("controlsHIDDEN")) {
+    closeBetBar();
+    openBalanceBar();
+  }
+  else
+    closeBalanceBar();
+}
 
 function toggleBetControls() {
   if (betControls.classList.contains("controlsHIDDEN")) {
-    if (!balanceControls.classList.contains("controlsHIDDEN")) {
-      balanceControls.classList.add("controlsHIDDEN");
-      userBalanceBtn.classList.remove("userBalanceBtnACTIVE");
-      userBalanceAmountContainer.style.animation = "none";
-    }
-    betControls.classList.remove("controlsHIDDEN");
-    userBetAmountContainer.style.animation = "balanceControlsEntrance forwards ease-out .3s"
-    userBetBtn.classList.add("userBalanceBtnACTIVE");
+    closeBalanceBar();
+    openBetBar();
   }
-  else {
-    betControls.classList.add("controlsHIDDEN");
-    userBetBtn.classList.remove("userBalanceBtnACTIVE");
-    userBetAmountContainer.style.animation = "none";
-  }
+  else
+    closeBetBar();
+}
+
+function openBalanceBar() {
+  // Open bar
+  balanceControls.classList.remove("controlsHIDDEN");
+  userBalanceAmountContainer.style.animation = barAnimation;
+  userBalanceBtn.classList.add("userBalanceBtnACTIVE");
+  // Buttons animation
+  balanceButtons.forEach((button, index) => {
+    button.style.animation = buttonsAnimation;
+    button.style.animationDelay = `${(1 + index) * 0.07}s`;
+  });
+}
+
+function closeBalanceBar() {
+  balanceButtons.forEach((button, index) => {
+    button.style.animation = "none";
+  });
+  balanceControls.classList.add("controlsHIDDEN");
+  userBalanceBtn.classList.remove("userBalanceBtnACTIVE");
+  userBalanceAmountContainer.style.animation = "none";
+}
+
+function openBetBar() {
+  // Open bar
+  betControls.classList.remove("controlsHIDDEN");
+  userBetAmountContainer.style.animation = barAnimation;
+  userBetBtn.classList.add("userBalanceBtnACTIVE");
+  // Buttons animation
+  betButtons.forEach((button, index) => {
+    button.style.animation = buttonsAnimation;
+    button.style.animationDelay = `${(1 + index) * 0.07}s`;
+  });
+}
+
+function closeBetBar() {
+  betButtons.forEach((button, index) => {
+    button.style.animation = "none";
+  });
+  betControls.classList.add("controlsHIDDEN");
+  userBetBtn.classList.remove("userBalanceBtnACTIVE");
+  userBetAmountContainer.style.animation = "none";
 }

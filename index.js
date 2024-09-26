82d9414a1046c8c607abd0335e8fea74e5a436c6
Luca <luca.ambrosonee@gmail.com>
2024-09-26 17:57:19 +0200

    Fixed negative appStartBalance

--- index.js
+++ index.js
@@ -782,7 +782,11 @@ const balanceAtAppStartInput = document.getElementById("balanceAtAppStartInput")
 
 function changeBalanceAtAppStart() {
   const enteredData = parseFloat(balanceAtAppStartInput.value);
-  if (!isNaN(enteredData)) appData.appStartBalance = enteredData;
+  if (!isNaN(enteredData) && enteredData >= 0) appData.appStartBalance = enteredData;
+  else {
+    appData.appStartBalance = 150;
+    balanceAtAppStartInput.value = 150;
+  }
   saveAppData();
 }
 
